6eb75343d7dfc1cd09085def4077764a91ddbb44
Francesca <fmartone@coasit.com.au>
2026-04-24 14:02:09 +1000

    first program - guessing game

---
+++ hello.py
@@ -0,0 +1,25 @@
+import random
+
+
+name = input ("What's your name?") 
+
+
+secret = random.randint(1, 10)
+guess = 0
+attempts = 0
+
+while attempts <= 3:
+    guess = int(input("Guess the number between 1 and 10: "))
+    attempts += 1
+    
+    if guess == secret:
+        print(f"you've got it in {attempts}!")
+        break
+    elif guess < secret:
+        print("too low!")
+    else:
+        print("too high")
+
+
+if guess != secret:
+    print(f"sorry, the numeber was {secret}")
