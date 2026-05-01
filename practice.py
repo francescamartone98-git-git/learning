9d6833bb2ada6018c1563bca1d1c904344b32b24
Francesca <fmartone@coasit.com.au>
2026-05-01 12:32:30 +1000

    contact book with lists and dictionaries

--- practice.py
+++ practice.py
@@ -1,11 +1,30 @@
-shopping = []
+contacts = []
 
-item = ""
-while item != "done":
-    item = input("Add an item (or type 'done' to finish): ")
-    if item != "done":
-        shopping.append(item)
+while True:
+    print("\n1. Add contact")
+    print("2. Show all contacts")
+    print("3. Quit")
+    
+    choice = input("Choose an option: ")
+    
+    if choice == "3":
+        print("Goodbye!")
+        break
 
-print(f"\nYou have {len(shopping)} items:")
-for counter, item in enumerate(shopping, 1):
-    print(f"{counter}. {item}")
+    if choice == "1":
+        adding = True
+        while adding:
+            name = input("Name: ")
+            city = input("City: ")
+            contacts.append({"name": name, "city": city})
+            print(f"{name} added!")
+            another = input("Add another? (yes/no): ")
+            if another != "yes":
+                adding = False
+    
+    if choice == "2":
+        if len(contacts) == 0:
+            print("No contacts yet!")
+        else:
+            for person in contacts:
+                print(f"{person['name']} - {person['city']}")  
